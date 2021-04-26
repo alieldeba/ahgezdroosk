55a67069f693f507671656a2764f65f8d5d7ea12
Myo Naing Winn <myonaingwin.gic@gmail.com>
2021-04-26 14:42:08 +0630

    NinjaList link updated

--- components/navbar.js
+++ components/navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
             </div>
             <Link href='/'><a>Home</a></Link>
             <Link href='/about'><a>About</a></Link>
-            <Link href='/ninja/list'><a>Ninja List</a></Link>
+            <Link href='/ninja'><a>Ninja List</a></Link>
         </nav>
      );
 }
