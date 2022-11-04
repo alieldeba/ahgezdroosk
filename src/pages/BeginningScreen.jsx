2254714dff75961f8a3cac77b45cb75999548a06
Ali Eldeba <alieldeba375@gmail.com>
2022-11-04 15:23:03 +0200

    Update BeginningScreen.jsx

--- src/pages/BeginningScreen.jsx
+++ src/pages/BeginningScreen.jsx
@@ -5,16 +5,16 @@ export default function BeginningScreen() {
     <section className="text-center h-screen flex flex-col justify-center items-center">
       <h1 className="text-3xl mb-3">مرحبا بكم فى منصة احجز اونلاين</h1>
       <p className="text-lg">يرجى تسجيل الدخول او إنشاء حساب للبدء</p>
-      <div className="flex gap-3 mt-5">
+      <div className="flex mt-5">
         <Link
           to="/login"
-          className="px-5 py-2.5 text-white bg-indigo-600 duration-150 hover:bg-indigo-700 active:shadow-lg"
+          className="px-5 py-2.5 mr-2.5 text-white bg-indigo-600 duration-150 hover:bg-indigo-700 active:shadow-lg"
         >
           تسجيل الدخول
         </Link>
         <Link
           to="/signup"
-          className="px-5 py-2.5 text-white bg-indigo-600 duration-150 hover:bg-indigo-700 active:shadow-lg"
+          className="px-5 py-2.5 ml-2.5 text-white bg-indigo-600 duration-150 hover:bg-indigo-700 active:shadow-lg"
         >
           إنشاء حساب
         </Link>
