a0025073926a7015369d1f6b050544ec88bbea70
Ali Eldeba <alieldeba375@gmail.com>
2022-11-05 11:07:50 +0200

    Update BeginningScreen.jsx

--- src/pages/BeginningScreen.jsx
+++ src/pages/BeginningScreen.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 export default function BeginningScreen() {
   return (
     <section className="text-center h-screen flex flex-col justify-center items-center">
-      <h1 className="text-3xl mb-3">مرحبا بكم فى منصة احجز اونلاين</h1>
-      <p className="text-lg">يرجى تسجيل الدخول او إنشاء حساب للبدء</p>
+      <h1 className="text-xl lg:text-3xl mb-3">مرحبا بكم فى منصة احجز اونلاين</h1>
+      <p className="text-md lg:text-lg">يرجى تسجيل الدخول او إنشاء حساب للبدء</p>
       <div className="flex mt-5">
         <Link
           to="/login"
