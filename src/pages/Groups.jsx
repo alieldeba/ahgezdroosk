4865ac0e7d51bce385cebea8ed70f80356e79c94
Ali Eldeba <alieldeba375@gmail.com>
2022-11-05 10:59:15 +0200

    Update Groups.jsx

--- src/pages/Groups.jsx
+++ src/pages/Groups.jsx
@@ -5,7 +5,7 @@ export default function Groups() {
     <>
       <h1 className="text-center text-2xl p-10">المجموعات</h1>
       {/* Groups section */}
-      <div className="mt-5 grid gap-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+      <div className="mt-5 grid gap-2 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
         <GroupCard title="مجموعة عبد العال حد و اربع الساعة اربعة للصف الثالث الإعدادى" />
         <GroupCard title="مجموعة عبد العال حد و اربع الساعة اربعة للصف الثالث الإعدادى" />
         <GroupCard title="مجموعة عبد العال حد و اربع الساعة اربعة للصف الثالث الإعدادى" />
