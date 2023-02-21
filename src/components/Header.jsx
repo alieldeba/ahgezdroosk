04d22c628a6c88ca31d3f50af0a6fb0745e4aa6e
Ali-Eldeba <alieldeba375@gmail.com>
2023-02-21 21:46:15 +0200

    fixing bugs

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -70,7 +70,7 @@ function Header() {
           </li>
           <div className="order-1 flex-1 justify-center items-center space-y-5 md:flex md:space-x-6 md:space-x-reverse md:space-y-0">
             {navigation.map((item, idx) => (
-              <li className="text-gray-500" key={idx}>
+              <li className="text-gray-500 hover:text-black" key={idx}>
                 <Link to={item.path}>{item.title}</Link>
               </li>
             ))}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,24 +2,26 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    container: {
-      center: true,
-      padding: {
-        DEFAULT: "1rem",
+    extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+        },
       },
+      colors: {
+        primary: "#4f46e5",
+        primarysoft: "#0069e5",
+        secondary: "#848b95",
+        selection: "#ffcc66db",
+        success: "#157347",
+        danger: "#dc3545",
+        dark: "#1f2937",
+        white: "#fff",
+        black: "#000",
+      },
+    
     },
-    colors: {
-      primary: "#4f46e5",
-      primarysoft: "#0069e5",
-      secondary: "#848b95",
-      selection: "#ffcc66db",
-      success: "#157347",
-      danger: "#dc3545",
-      dark: "#1f2937",
-      white: "#fff",
-      black: "#000",
-    },
-    extend: {},
   },
   plugins: [],
 };
